cdff5c2bd1c476d66f4531303060637c9bf6c5e4
Kbidros <kristianbidros@gmail.com>
2023-10-03 21:48:08 +0200

    Uppgift 1

--- src/com/kristian/demo/Main.java
+++ src/com/kristian/demo/Main.java
@@ -3,7 +3,13 @@ package com.kristian.demo;
 public class Main {
     public static void main(String[] args) {
 
+        for (int i = 0; i < 5; i++) {
 
+            System.out.println("You're placed in line, you're number: " + i);
+
+
+
+        }
 
         }
 }
